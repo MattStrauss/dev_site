cbf16a530aa61c565210c98692f464c292fb0e17
Matt Strauss <strausstester@gmail.com>
2022-01-08 16:20:42 -0500

    Fix Image Links

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -54,7 +54,7 @@ class PageController extends Controller
                     'name' => 'MenuCrunch',
                     'skills' => ['laravel', 'alpineJS', 'tailwindCss'],
                     'link' => 'https://menucrunch.com',
-                    'img' => '/images/menucrunch.png',
+                    'img' => '/storage/images/menucrunch.png',
                     'demo'  => 'https://screencast-o-matic.com/watch/crf0X6VelBg',
                     'description' => "Allows restaurants to quickly and easily get their menu online. This app also helps restaurants accept and streamline online orders and has powerful analysis tools."
                 ],
@@ -62,7 +62,7 @@ class PageController extends Controller
                     'name' => 'Tic-Tac-Toe',
                     'skills' => ['assembly'],
                     'link' => 'https://github.com/MattStrauss/tictactoe',
-                    'img' => '/images/tic_tac_toe.png',
+                    'img' => '/storage/images/tic_tac_toe.png',
                     'demo'  => 'https://screencast-o-matic.com/watch/crfUlvVea9L',
                     'description' => "An assembly version of Tic-Tac-Toe whereas you compete against the machine. The machine is semi-intelligent, which considering this program was written in assembly, was not easy to pull off."
                 ],
@@ -70,7 +70,7 @@ class PageController extends Controller
                     'name' => 'Polls',
                     'skills' => ['django', 'python', 'tailwindCss'],
                     'link' => 'https://github.com/MattStrauss/polls',
-                    'img' => '/images/polls.png',
+                    'img' => '/storage/images/polls.png',
                     'description' => "This is an enhanced version of the Polls app from the Django tutorial docs. It was built for polls at my undergrad college, so students could vote for their favorite prof, class etc."
 
                 ],
@@ -78,7 +78,7 @@ class PageController extends Controller
                     'name' => 'SWENG Scheduler',
                     'skills' => ['laravel', 'reactJS', 'd3js', 'tailwindCss'],
                     'link' => 'https://psuswengscheduler.com/',
-                    'img' => '/images/sweng.png',
+                    'img' => '/storage/images/sweng.png',
                     'demo'  => 'https://screencast-o-matic.com/watch/crjQbsVjDJT',
                     'description' => "A tool to help students find course information and get course recommendations for the Software Engineering curriculum at the Pennsylvania State University."
                 ],
@@ -86,7 +86,7 @@ class PageController extends Controller
                     'name' => 'Tetris',
                     'skills' => ['java'],
                     'link' => 'https://github.com/MattStrauss/tetris',
-                    'img' => '/images/tetris.png',
+                    'img' => '/storage/images/tetris.png',
                     'demo'  => 'https://screencast-o-matic.com/watch/crjDIXVj4DV',
                     'description' => "A java version of the classic game of Tetris. Includes a preview of the upcoming piece and a score keeper."
                 ],
@@ -94,7 +94,7 @@ class PageController extends Controller
                     'name' => 'My Dev Site',
                     'skills' => ['laravel', 'inertiaJS', 'vueJS', 'tailwindCss'],
                     'link' => 'https://mattstrauss.dev',
-                    'img' => '/images/devsite.png',
+                    'img' => '/storage/images/devsite.png',
                     'demo'  => null,
                     'description' => "The site you are looking at right now! It was built with the amazing Inertia.js framework, which I highly recommend checking out."
                 ],
@@ -102,7 +102,7 @@ class PageController extends Controller
                     'name' => 'Swift Grocery List',
                     'skills' => ['laravel', 'vueJS', 'bootstrap4'],
                     'link' => 'https://swiftgrocerylist.com',
-                    'img' => '/images/sgl.png',
+                    'img' => '/storage/images/sgl.png',
                     'demo'  => null,
                     'description' => "A web application that helps you create a grocery shopping list fast. Built with Laravel and Vue.js. Includes recipes, drag-and-drop aisle sorting and much more."
                 ],
@@ -110,7 +110,7 @@ class PageController extends Controller
                     'name' => 'Dresser',
                     'skills' => ['laravel', 'larevel livewire', 'tailwindCss'],
                     'link' => 'https://github.com/MattStrauss/dresser',
-                    'img' => '/images/dresser.png',
+                    'img' => '/storage/images/dresser.png',
                     'demo'  => null,
                     'description' => "A quick and (relatively) dirty mock project to try out Laravel Livewire. The idea was to create a UI that would typically require a decent amount of JS, but to only use Livewire and write zero JS. " .
                         "This is heavily inspired by Caleb Porzio's Guest Spotlight episode on Laracasts where he built an interactive table. It's open source on github, so check it out!"
