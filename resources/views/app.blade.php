fd31c40037b77d95277d85552b725e756f3e2cd4
Matt Strauss <strausstester@gmail.com>
2022-03-06 07:27:22 -0500

    Add Highlight.js Code Syntax Highlighting

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,16 @@
 
     <script src="https://www.google.com/recaptcha/api.js?render=6Ld2bfYdAAAAAL6yv0Oa-lRgw9y93KtIaXDdo20T"></script>
     <style id="captchaStyle"> .grecaptcha-badge { visibility: hidden !important; } </style>
+    <link rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/agate.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            document.querySelectorAll('pre').forEach((el) => {
+                hljs.highlightElement(el);
+            });
+        });
+    </script>
 </head>
 <body class="bg-gray-700">
 @inertia
